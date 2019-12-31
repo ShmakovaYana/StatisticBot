52fcb48d33a5a6aa23f2e4a4a0732f8c9bcac197
Yana <yanaaaaaa1998@mail.ru>
2019-12-31 16:59:29 +0300

    Bot

--- src/main/java/BotAbility.java
+++ src/main/java/BotAbility.java
@@ -1,64 +1,68 @@
-import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.db.DBContext;
 import org.telegram.abilitybots.api.objects.*;
 import org.telegram.abilitybots.api.sender.SilentSender;
 import org.telegram.abilitybots.api.util.AbilityExtension;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import java.util.Arrays;
-import java.util.Map;
-import static org.telegram.abilitybots.api.util.AbilityUtils.stripTag;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.abilitybots.api.objects.Reply;
 
 public class BotAbility implements AbilityExtension {
     private SilentSender silent;
     private DBContext db;
     private User user;
+
     BotAbility(SilentSender silent, DBContext db) {
         this.silent = silent;
         this.db = db;
     }
 
-    public BotAbility(DefaultBotOptions botOptions) {
-    }
 
     public Reply AddPhoto() {
         return Reply.of(update -> {
-            long chatId = update.getMessage().getChatId();
-            long userId = user.getId();
-            Stat stat = new Stat(db, chatId, userId);
-            stat.newPhoto();
-            },
+                    long chatId = update.getMessage().getChatId();
+                    long userId = update.getMessage().getFrom().getId();
+                    Stat stat = new Stat(db, chatId, userId);
+                    stat.newPhoto();
+                },
                 Flag.PHOTO);
     }
-   public Reply AddMessage() {
+
+    public Reply AddMessage() {
         return Reply.of(update -> {
-            long chatId = update.getMessage().getChatId();
-            long userId = user.getId();
-            Stat stat = new Stat(db, chatId, userId);
-            stat.newMess();
-            },
+                    long chatId = update.getMessage().getChatId();
+                    long userId = update.getMessage().getFrom().getId();
+                    Stat stat = new Stat(db, chatId, userId);
+                    if (update.getMessage().getText().equals("/delete")) {
+                        stat.remove();
+                    } else if (update.getMessage().getText().equals("/statistic")) {
+                        String user=update.getMessage().getFrom().getUserName();
+                        silent.send(stat.get(user), update.getMessage().getChatId());
+                    } else if (update.getMessage().getText().equals("/statisticAll")) {
+                        silent.send(stat.getAll(), update.getMessage().getChatId());
+                    } else {
+                        stat.newMess();
+                    }
+                },
                 Flag.TEXT);
     }
+
     public Reply AddAnswer() {
         return Reply.of(update -> {
-            long chatId = update.getMessage().getChatId();
-            long userId = user.getId();
-            Stat stat = new Stat(db, chatId, userId);
-            stat.newReply();
-            },
+                    long chatId = update.getMessage().getChatId();
+                    long userId = update.getMessage().getFrom().getId();
+                    Stat stat = new Stat(db, chatId, userId);
+                    stat.newReply();
+                },
                 Flag.REPLY);
     }
-    public Reply OutStat() {
+    public Reply AddDoc() {
         return Reply.of(update -> {
-            long chatId = update.getMessage().getChatId();
-            long userId = user.getId();
-            Stat stat = new Stat(db, chatId, userId);
-            silent.send(stat.get(), update.getMessage().getChatId());
-            },
-                update -> update.getMessage().getText().equals("statistic"));
+                    long chatId = update.getMessage().getChatId();
+                    long userId = update.getMessage().getFrom().getId();
+                    Stat stat = new Stat(db, chatId, userId);
+                    stat.newDoc();
+                },
+                Flag.DOCUMENT);
     }
-  }
+
+}
--- src/main/java/Stat.java
+++ src/main/java/Stat.java
@@ -12,33 +12,41 @@ public class Stat {
 
         this.chatId = chatId;
         this.userId = userId;
+        boolean flag = false;
+
+        if (statMap.isEmpty()) {
+            statMap.put(statMap.size(), new long[] {chatId,0,0,0,0});
+        }
 
         for (int i : statMap.keySet()) {
-
-            if (statMap.get(i)[0] != chatId) {
-                statMap.put(statMap.size(), new long[] {chatId,0,0,0});
-            }
-            if (statMap.get(i)[0] != userId) {
-                statMap.put(statMap.size(), new long[] {userId,0,0,0});
+            long q =statMap.get(i)[0];
+            if (statMap.get(i)[0] == userId) {
+                flag = true;
             }
         }
+
+        if (!flag) {
+            statMap.put(statMap.size(), new long[] {userId,0,0,0,0});
+        }
     }
+
     void newMess () {
         for (int i : statMap.keySet()) {
+            long q = statMap.get(i)[0];
             if (statMap.get(i)[0] == (chatId)) {
-                statMap.put(statMap.size(), new long[] {
+                statMap.put(i, new long[] {
                         statMap.get(i)[0],
                         statMap.get(i)[1] + 1,
                         statMap.get(i)[2],
                         statMap.get(i)[3],
+                        statMap.get(i)[4]
                 });
-            }
-            if (statMap.get(i)[0] == (userId)) {
-                statMap.put(statMap.size(), new long[] {
-                        statMap.get(i)[0],
+            } else if (statMap.get(i)[0] == (userId)) {
+                statMap.put(i, new long[] {statMap.get(i)[0],
                         statMap.get(i)[1] + 1,
                         statMap.get(i)[2],
                         statMap.get(i)[3],
+                        statMap.get(i)[4]
                 });
             }
         }
@@ -47,55 +55,106 @@ public class Stat {
     void newPhoto () {
         for (int i : statMap.keySet()) {
             if (statMap.get(i)[0] == (chatId)) {
-                statMap.put(statMap.size(), new long[] {
+                statMap.put(i, new long[] {
                         statMap.get(i)[0],
                         statMap.get(i)[1],
                         statMap.get(i)[2] + 1,
                         statMap.get(i)[3],
+                        statMap.get(i)[4]
                 });
             }
-            if (statMap.get(i)[0] == (userId)) {
-                statMap.put(statMap.size(), new long[]{
+            else if (statMap.get(i)[0] == (userId)) {
+                statMap.put(i, new long[]{
                         statMap.get(i)[0],
                         statMap.get(i)[1],
                         statMap.get(i)[2] + 1,
                         statMap.get(i)[3],
+                        statMap.get(i)[4]
                 });
             }
         }
     }
+
     void newReply () {
         for (int i : statMap.keySet()) {
             if (statMap.get(i)[0] == (chatId)) {
-                statMap.put(statMap.size(), new long[] {
+                statMap.put(i, new long[] {
                         statMap.get(i)[0],
                         statMap.get(i)[1],
                         statMap.get(i)[2],
                         statMap.get(i)[3] + 1,
+                        statMap.get(i)[4]
                 });
-            }
-            if (statMap.get(i)[0] == (userId)) {
-                statMap.put(statMap.size(), new long[]{
+            } else if (statMap.get(i)[0] == (userId)) {
+                statMap.put(i, new long[]{
                         statMap.get(i)[0],
-                        statMap.get(i)[1], statMap.get(i)[2],
+                        statMap.get(i)[1],
+                        statMap.get(i)[2],
                         statMap.get(i)[3] + 1,
+                        statMap.get(i)[4]
                 });
             }
         }
     }
-    String get() {
+    void newDoc () {
+        for (int i : statMap.keySet()) {
+            if (statMap.get(i)[0] == (chatId)) {
+                statMap.put(i, new long[] {
+                        statMap.get(i)[0],
+                        statMap.get(i)[1],
+                        statMap.get(i)[2],
+                        statMap.get(i)[3] ,
+                        statMap.get(i)[4]+ 1
+                });
+            } else if (statMap.get(i)[0] == (userId)) {
+                statMap.put(i, new long[]{
+                        statMap.get(i)[0],
+                        statMap.get(i)[1],
+                        statMap.get(i)[2],
+                        statMap.get(i)[3] ,
+                        statMap.get(i)[4]+ 1
+                });
+            }
+        }
+    }
+
+
+    String get(String user) {
         String a = "Пусто";
         for (int i : statMap.keySet()) {
             if (statMap.get(i)[0] == (userId)) {
-                a = "Всего:\n"
-                        + statMap.get(i)[1]
-                        + "сообщений\n"
-                        + statMap.get(i)[2]
-                        + "фото\n"
-                        + statMap.get(i)[3]
-                        + "ответов\n";
+                a = "Всего у " +user+":\n"+
+                        statMap.get(i)[1] + "сообщений\n" +
+                        statMap.get(i)[2] + "фото\n" +
+                        statMap.get(i)[3] + "ответов\n"+
+                        statMap.get(i)[4]+"документов\n";
             }
         }
         return a;
+    }
+
+    String getAll() {
+        String a;
+        int mess = 0;
+        int photo = 0;
+        int reply = 0;
+        int doc = 0;
+        for (int i : statMap.keySet()) {
+            mess += statMap.get(i)[1];
+            photo += statMap.get(i)[2];
+            reply += statMap.get(i)[3];
+            doc += statMap.get(i)[4];
+        }
+        a = "Всего:\n" + mess + "сообщений\n"
+                + photo + "фото\n"
+                + reply + "ответов\n"
+                + doc + "документов\n";
+        return a;
+    }
+
+    void remove () {
+        for (int i : statMap.keySet()) {
+            statMap.remove(i);
+        }
     }
 }
